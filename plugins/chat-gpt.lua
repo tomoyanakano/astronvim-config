b188a5c6929120a833850bb7557148f2f742f0ec
tomoyanakano <tomo1999724@gmail.com>
2024-02-23 18:42:26 +0900

    feat(chat-gpt): Add chat-gpt plugin with dependencies

---
+++ plugins/chat-gpt.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "jackMort/ChatGPT.nvim",
+    event = "VeryLazy",
+    config = function()
+      require("chatgpt").setup {
+        // Load Open AI access token from 1Password
+        api_key_cmd = "op read op://Personal/open_ai/key --no-newline",
+      }
+    end,
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "nvim-lua/plenary.nvim",
+      "folke/trouble.nvim",
+      "nvim-telescope/telescope.nvim",
+    },
+  },
+}
